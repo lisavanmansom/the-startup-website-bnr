6789502c5451b0b7cda7db9702e892ffcbb33a02
lisavanmansom <lisavanmansom@gmail.com>
2024-01-17 12:19:36 +0100

    micro interactie + beeldmateriaal aanpassing + app css

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-// const playradio = document.getElementsByClassName("play-radio");
-// function playRadio() {
-//     let audio = new Audio ("https://27823.live.streamtheworld.com/BNR_NIEUWSRADIO.mp3?dist=bnrweb");
-//     audio.play()
-// }
-
-// playradio.addEventListener("click", () => {
-//     playRadio("test")
-// });
-
-
-
-
-
     // 1. Selecteren van element via class
-    const playradio = document.getElementsByClassName('play-radio')
-    const audio = new Audio ("https://27823.live.streamtheworld.com/BNR_NIEUWSRADIO.mp3?dist=bnrweb");
+    const playradio = document.querySelector('.play-radio');
+    const audio = document.querySelector('audio');
 
     // 2. Event
-
-    playradio.addEventListener('click', () => {
-        audio.play("test1");
-    });
+     playradio.addEventListener('click', () => {
+            audio.play();
+        });
 
     playradio.onplay = () => {
-        console.log("test test");
+        console.log();
     };
+
 
